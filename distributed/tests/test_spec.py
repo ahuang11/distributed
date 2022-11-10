2026466b330359a144e2def6e293166669372ce4
ahuang11 <andrew.h@prefect.io>
2022-11-09 17:12:49 -0800

    Add rough testing for `close_clusters` behavior

--- distributed/tests/test_spec.py
+++ distributed/tests/test_spec.py
@@ -2,6 +2,15 @@ from __future__ import annotations
 
 from distributed.deploy.spec import ProcessInterface
 from distributed.utils_test import gen_test
+import uuid
+from unittest.mock import MagicMock
+
+from distributed.deploy.spec import (
+    ProcessInterface,
+    SpecCluster,
+    Status,
+    close_clusters,
+)
 
 
 @gen_test()
@@ -22,3 +31,29 @@ async def test_child_address_persists():
 
     async with Child("localhost") as c:
         assert c.address == "localhost"
+    c = Child()
+    assert c.address is None
+    c = Child("localhost")
+    assert c.address == "localhost"
+
+
+def test_close_clusters(monkeypatch):
+    def mock_cluster(status: Status, shutdown_on_close: bool = True):
+        mock = MagicMock(spec=SpecCluster)
+        mock.status = status
+        mock.shutdown_on_close = shutdown_on_close
+        return mock
+
+    closed_cluster = mock_cluster(Status.closed)
+    skipped_cluster = mock_cluster(Status.running, shutdown_on_close=False)
+    running_cluster = mock_cluster(Status.running)
+
+    monkeypatch.setattr(SpecCluster, "_instances", {running_cluster, closed_cluster})
+
+    close_clusters()
+
+    closed_cluster.sync.assert_not_called()
+    skipped_cluster.sync.assert_not_called()
+    running_cluster.sync.assert_called_once_with(
+        running_cluster.close, asynchronous=False, callback_timeout=10
+    )
